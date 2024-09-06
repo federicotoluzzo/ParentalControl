4f6c2208bbc04d1550665d664a7b6e071ba1d786
federicotoluzzo <federico.toluzzo1@gmail.com>
2024-09-06 17:26:31 +0000

    First? commit

---
+++ src/Timer.kt
@@ -0,0 +1,61 @@
+import org.w3c.dom.events.EventListener
+import java.awt.Color
+import java.awt.FlowLayout
+import java.awt.Font
+import java.awt.event.KeyEvent
+import java.awt.event.KeyListener
+import java.lang.Thread.sleep
+import javax.swing.JFrame
+import javax.swing.JLabel
+
+class Timer : JFrame{
+
+    var startTime: Long = 0
+    val duration: Long
+    var endTime: Long = 0
+
+    var showTime = JLabel()
+
+    constructor(duration: Long) {
+        this.duration = duration
+    }
+
+    fun start() {
+        startTime = System.currentTimeMillis()
+        endTime = startTime + duration
+        add(showTime)
+        isUndecorated = true
+        isVisible = true
+        layout = FlowLayout()
+        background = Color(255,255,255, 128)
+        showTime.foreground = Color.BLACK
+        showTime.font = Font("Timer Font", Font.BOLD, 32)
+        isAlwaysOnTop = true
+        pack()
+
+        Thread{
+            while(true){
+                showTime.text = getClock()
+                sleep(1000)
+                pack()
+            }
+        }.start()
+    }
+
+    fun getClock():String{
+        val secondsLeft:Long
+        val minutesLeft:Long
+        val hoursLeft:Long
+
+        var timeLeft = endTime - System.currentTimeMillis();
+        timeLeft /= 1000 //get the number of seconds left
+
+        secondsLeft = timeLeft % 60
+        timeLeft /= 60
+        minutesLeft = timeLeft % 60
+        timeLeft /= 60
+        hoursLeft = timeLeft % 60
+
+        return "$hoursLeft:$minutesLeft:$secondsLeft"
+    }
+}
