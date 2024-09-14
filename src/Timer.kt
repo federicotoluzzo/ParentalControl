3b4730e663ed0e5434cfa262e5a3e92bbfb1f23a
federicotoluzzo <federico.toluzzo1@gmail.com>
2024-09-14 11:12:57 +0200

    Made the screen lock nicer

--- src/Timer.kt
+++ src/Timer.kt
@@ -3,8 +3,11 @@ import java.awt.*
 import java.awt.event.KeyEvent
 import java.awt.event.KeyListener
 import java.lang.Thread.sleep
+import javax.swing.BoxLayout
+import javax.swing.JButton
 import javax.swing.JFrame
 import javax.swing.JLabel
+import kotlin.system.exitProcess
 
 class Timer : JFrame{
 
@@ -55,15 +58,38 @@ class Timer : JFrame{
         timeLeft /= 60
         hoursLeft = timeLeft % 60
 
-        return "$hoursLeft:$minutesLeft:$secondsLeft"
+        var clock = ""
+        clock += if (hoursLeft < 10) "0$hoursLeft" else hoursLeft
+        clock += ":"
+        clock += if (minutesLeft < 10) "0$minutesLeft" else minutesLeft
+        clock += ":"
+        clock += if (secondsLeft < 10) "0$secondsLeft" else secondsLeft
+
+        return clock
     }
 
     fun lockScreen(){
         val lock = JFrame()
+        val lockText = JLabel()
+        val shutdownButton = JButton()
+
         lock.isUndecorated = true
+        lock.layout = FlowLayout() /*BoxLayout(lock, BoxLayout.Y_AXIS)*/
         lock.background = Color(255,0,0, 128)
         lock.size = Dimension(Toolkit.getDefaultToolkit().screenSize.width, Toolkit.getDefaultToolkit().screenSize.height)
         lock.isAlwaysOnTop = true
+
+        lockText.font = Font("Arial", Font.BOLD, 128)
+        lockText.text = "put json languages here"
+
+        shutdownButton.text = "put json languages here"
+        shutdownButton.addActionListener {
+            Runtime.getRuntime().exec("shutdown -s")
+        }
+
+        lock.add(lockText)
+        lock.add(shutdownButton)
+
         lock.isVisible = true
     }
 }
